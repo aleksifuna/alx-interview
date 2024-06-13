abf7e7eb6c04c3f96c3e3ab5e41e9ff434b966bf
Alex Sifuna <aleksifuna@gmail.com>
2024-06-13 12:11:10 +0300

    added a function that calculates the perimeter of a grid

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -13,12 +13,16 @@ def island_perimeter(grid):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 perimeter += 4
-                if grid[i][j + 1] == 1:
-                    perimeter -= 1
-                if grid[i][j - 1] == 1:
-                    perimeter -= 1
-                if grid[i + 1][j] == 1:
-                    perimeter -= 1
-                if grid[i - 1][j] == 1:
-                    perimeter -= 1
+                if j != len(grid[i]) - 1:
+                    if grid[i][j + 1] == 1:
+                        perimeter -= 1
+                if j != 0:
+                    if grid[i][j - 1] == 1:
+                        perimeter -= 1
+                if i != len(grid) - 1:
+                    if grid[i + 1][j] == 1:
+                        perimeter -= 1
+                if i != 0:
+                    if grid[i - 1][j] == 1:
+                        perimeter -= 1
     return perimeter
